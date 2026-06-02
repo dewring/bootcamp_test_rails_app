81f345bd8f55d56c7883822d711d8b0f4043e63c
Ellie Kweon <kweondew@gmail.com>
2026-06-02 16:07:15 -0700

    create teacher model

---
+++ app/models/teacher.rb
@@ -0,0 +1,5 @@
+class Teacher < ApplicationRecord
+  has_many :school_classes, dependent: :nullify
+  has_many :registrations, through: :school_classes
+  has_many :students, through: :registrations
+end
