e919a11453552a5262489bbdd277def5c26c97cc
Ellie Kweon <kweondew@gmail.com>
2026-06-03 14:55:37 -0700

    test teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,6 @@
 class Teacher < ApplicationRecord
+  validates :name, presence: true
+
   has_many :school_classes, dependent: :nullify
   has_many :registrations, through: :school_classes
   has_many :students, through: :registrations
