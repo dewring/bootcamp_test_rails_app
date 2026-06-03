a8841a2f2ce588aca6f82d5a47f0f28ef8feabb2
Ellie Kweon <kweondew@gmail.com>
2026-06-03 14:55:58 -0700

    test student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  validates :name, presence: true
+  validates :grade, numericality: { greater_than: 0 }
+  validates :term, inclusion: { in: [ "first", "second" ] }
+
   has_many :registrations, dependent: :destroy
   has_many :school_classes, through: :registrations
   has_many :teachers, through: :school_classes
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name must be not empty" do
+    student = Student.create(name: "Leika", grade: 1, term: "first")
+    # puts student.errors.full_messages
+    assert student.valid?
+
+    invalid_student = Student.create(name: "", grade: 1, term: "first")
+    assert invalid_student.invalid?
+  end
+  test "grade must be more than 0" do
+    student = Student.create(name: "Tuna", grade: 19, term: "second")
+    assert student.valid?
+
+    invalid_student = Student.create(name: "Tuna", grade: -3, term: "second")
+    assert invalid_student.invalid?
+  end
+  test "term must be either first or second" do
+    student = Student.create(name: "Tuna", grade: 19, term: "second")
+    assert student.valid?
+
+    invalid_student = Student.create(name: "Tuna", grade: 19, term: "fifth")
+    assert invalid_student.invalid?
+  end
 end
