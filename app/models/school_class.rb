b6e000d332993d55afb83050cce29a881b554cb2
Ellie Kweon <kweondew@gmail.com>
2026-06-03 14:55:49 -0700

    test school class model

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,4 +1,7 @@
 class SchoolClass < ApplicationRecord
+  validates :subject, presence: true
+  validates :teacher_id, presence: true
+
   belongs_to :teacher
   has_many :registrations, dependent: :destroy
   has_many :students, through: :registrations
