9cdd2c34948b6f94d11428bce822a68bdb6dc7ed
Ellie Kweon <kweondew@gmail.com>
2026-06-04 13:50:52 -0700

    create registrations classes controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,32 @@
+class RegistrationsController < ApplicationController
+  def index
+    @registrations = Registration.all
+
+    respond_to do |format|
+      format.json do
+        render json: @registrations.map do |r|
+          { id: r.id,
+            student_id: r.student_id,
+            school_class_id: r.school_class_id,
+            point: r.point, booster: r.booster }
+        end
+      end
+    end
+  end
+
+  def show
+    @registration = Registration.find(params[:id])
+
+    respond_to do |format|
+      format.json do
+        render json: {
+          id: @registration.id,
+          student_name: @registration.student.name,
+          subject: @registration.school_class.subject,
+          point: @registration.point,
+          booster: @registration.booster
+        }
+      end
+    end
+  end
+end
