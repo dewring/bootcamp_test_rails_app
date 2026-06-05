0bf7fe1b7c4df070f5ca059fc2aa8c54faf96f43
Ellie Kweon <kweondew@gmail.com>
2026-06-04 18:55:54 -0700

    test controller

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index html displays registrations" do
+    get "/registrations"
+    assert_response :success
+    assert @response.body.include?("registrations")
+  end
+
+  test "index returns registrations as json array" do
+    get "/registrations.json"
+    assert_response :success
+
+    json = JSON.parse(@response.body)
+    assert_equal 80, json.first["point"]
+    assert_equal 10, json.last["booster"]
+  end
+
+  test "show html loads successfully" do
+    registration = Registration.first
+    get "/registrations/#{registration.id}"
+    assert_response :success
+  end
+
+  test "show returns correct registration json data" do
+    registration = Registration.first
+    get "/registrations/#{registration.id}.json"
+    assert_response :success
+
+    json = JSON.parse(@response.body)
+    # assert_equal registration.id, json["id"]
+    assert_equal 80, json["point"]
+    assert_equal 20, json["booster"]
+    assert_equal "Chiikawa", json["student_name"]
+    assert_equal "being cute", json["subject"]
+  end
 end
--- test/controllers/school_classes_controller_test.rb
+++ test/controllers/school_classes_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index html displays school class" do
+    get "/school_classes"
+    assert_response :success
+    assert @response.body.include?("school_classes")
+  end
+
+  test "index returns school classes as json array" do
+    get "/school_classes.json"
+    assert_response :success
+
+    json = JSON.parse(@response.body)
+    assert_equal "hunting", json.last["subject"]
+  end
+  test "show html loads successfully" do
+    school_class = SchoolClass.find_by(subject: "being cute")
+
+    get "/school_classes/#{school_class.id}"
+    assert_response :success
+  end
+
+  test "show returns correct school class json data" do
+    school_class = SchoolClass.find_by(subject: "being cute")
+
+    get "/school_classes/#{school_class.id}.json"
+    assert_response :success
+
+    json = JSON.parse(@response.body)
+    assert_equal "being cute", json["subject"]
+    assert_equal "Jaina", json["teacher_name"]
+    assert_equal "Usagi", json["students"].last
+  end
 end
