75ba106984aeb78853ccdc4b7815641f8ef56fd7
Ellie Kweon <kweondew@gmail.com>
2026-06-08 16:18:09 -0700

    add helper methods for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,20 @@ class User < ApplicationRecord
   before_validation :ensure_api_token, on: :create
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :role, inclusion: { in: [ "teacher", "admin" ] }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :teacher
+
   def ensure_api_token
     self.api_token ||= SecureRandom.hex(20)
+  end
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def teacher?
+    self.role == "teacher"
   end
 end
